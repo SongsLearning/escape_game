5e179208566aae6bbecab6ba0e67c3e2ed7715d9
jhy068139 <64052098+jhy068139@users.noreply.github.com>
2020-11-30 09:49:34 +0900

    game first
    엔딩장면+설명장면 필요

---
+++ escape/Assets/script/Scene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    public void MoveMain()
+    {
+        SceneManager.LoadScene("Main_game");
+    }
+
+    public void MoveStart()
+    {
+        SceneManager.LoadScene("start");
+
+    }
+
+}
